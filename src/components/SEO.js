8ad0541ef275eb3fa2d6081f78950fd734455585
Romain Penaruiz <rom.penaruiz@gmail.com>
2020-06-29 18:38:32 -0400

    Seo done plus changed the pictured for my projects and fixed some css

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -2,8 +2,30 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-const SEO = () => {
-  return <div>SEO Component</div>
+export const query = graphql`
+  {
+    site {
+      siteMetadata {
+        author
+        siteDesc: description
+        siteUrl
+        siteTitle: title
+        image
+      }
+    }
+  }
+`
+
+const SEO = ({ title, description }) => {
+  const { site } = useStaticQuery(query)
+
+  const { author, siteDesc, siteUrl, siteTitle, image } = site.siteMetadata
+  return (
+    <Helmet title={` ${title} | ${siteTitle}`} htmlAttribute={{ lang: "en" }}>
+      <meta name="description" content={description || siteDesc} />
+      <meta name="image" content={image} />
+    </Helmet>
+  )
 }
 
 export default SEO
